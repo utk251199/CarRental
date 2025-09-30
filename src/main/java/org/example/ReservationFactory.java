dbf8957ce35591222da0ceafdc866a9bda6dadae
utk251199 <utkarshbhadauria251199@gmail.com>
2025-10-01 00:42:45 +0530

    Improvements

--- src/main/java/org/example/ReservationFactory.java
+++ src/main/java/org/example/ReservationFactory.java
@@ -5,13 +5,3 @@ import java.time.LocalDate;
 public interface ReservationFactory {
     Reservation createReservation(Person person, Vehicle vehicle, LocalDate startDate, LocalDate endDate);
 }
-
-// Concrete implementation
-//public class DefaultReservationFactory implements ReservationFactory {
-//    @Override
-//    public Reservation createReservation(Person person, Vehicle vehicle, LocalDate startDate, LocalDate endDate) {
-//        Reservation reservation = new Reservation();
-//        reservation.createReservation(person, vehicle, startDate, endDate);
-//        return reservation;
-//    }
-//}
